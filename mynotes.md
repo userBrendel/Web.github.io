71afee39badf953f8b43dcca96f7fdf8ff4282bd
userBrendel <brendelcanlas@gmail.com>
2023-05-30 17:23:57 +0300

    .

---
+++ mynotes.md
@@ -0,0 +1,72 @@
+------- My goal is to finish this within 1 week ---------
+
+# This project requires 
+
+Pick a theme: Portfolio
+(own personal portfolio website that showcase
+my work, experience and overall development, something that I can use
+professionally in future.)
+
+
+-The piece must deploy HTML5 and CSS.
+-At least 5 pages where user can make navigational decisions.  (e.g. pick where they want
+to go next).
+-website should include media assets (i.e. still images, moving
+images, (audio or video if possible)) to make your website more
+interactive and engaging.
+-Javascript and related languages/technologies may be used to enhance
+your website
+
+# My plan/ idea
+
+1.Loading time/ introduction
+
+2.Header:
+-dark and light mode feature
+-logo
+-Introduction
+-navigation
+
+3.Main
+-About me
+-skills
+-project
+-goals
+-contact
+
+4.footer
+
+# Design
+-soft and chill like my personality!
+-3 colors! 
+   -#070606 (dark)
+   -#ed6b9a
+   -#00c1f2
+   -#fffffc (light)
+-a cute hover!
+-animations!
+
+
+
+--- gonna do my essay after finishing my website !! ----
+development document:
+
+-1000 words min
+● Brief: A very short description of what you have been asked to develop (50
+words).
+● Design Direction: What theme did you choose and why? What is the
+underlying concept of the piece? (200 words).
+● Interaction Overview: A description of how you intend the user to interact
+with your website example navigational decisions, control mechanisms
+(150 words).
+● Technical Description: A technical breakdown of how you created your
+project. Be sure to use correct terminology, remembering that this section
+evidences your understanding of the web development techniques used
+(300 words).
+● Critical Reflection: An evaluation of your project that notes what is
+compelling about the work, what could be improved, and what web
+programming techniques you need to learn to make such improvements
+(300 words)
+
+---------------------------------------------------------
+I'll do my best to finish this project! Go UserBrendel!!
---
+++ portfolio.js
@@ -0,0 +1,228 @@
+
+
+///----- LOADER SCRIPT ------///
+window.addEventListener("load", () => { // eventlistener that checks when the webpage load
+    const loader = document.querySelector(".loader"); 
+    document.body.style.overflow = "hidden"; // here i hide the scroll bar while loader is ongoing   
+    const content = document.getElementById("content");
+    
+    const delay = 4000; // making a variable that makes it 4sec delay to show class 'content'
+  
+    loader.classList.add("loader-hidden"); //The loader-hidden CSS would add in. This will make the loader gone
+  
+    // This is to  delay before showing the content
+    setTimeout(() => {
+      content.classList.remove("loader-hidden"); //put in the CSS class '.loader-hidden'
+      document.body.style.overflow = ""; // After the loader is hidden it will restore scrolling
+    }, delay);// the delay variable
+  
+    loader.addEventListener("transitionend", () => { //eventlistener when it transition
+      document.body.removeChild(document.querySelector(".loader")); // remove loader
+      document.body.style.overflow = ""; //show content
+  });
+  
+});
+
+//--------------------- MAKING A CREATIVE CURSOR  -----------------/
+
+document.addEventListener("mousemove", function(event) { // eventlistener 'mousemove'is typically use in this case
+  const cursor = document.querySelector(".cursor"); // defining cursor
+  // to be able to move
+  cursor.style.left = event.clientX + "px";
+  cursor.style.top = event.clientY + "px";
+});
+
+
+//-------------------- TYPE WRITER PLUGIN ----------//
+
+// i got this from github
+const typewriter = new Typewriter('.changetext', {
+    strings: ['UserBrendel', 'a Coder', 'a Designer',], // i change the strings
+    autoStart: true,
+    loop: true,
+});
+
+
+
+
+///----- ACTIVE NAV ITEM -----///
+//Here a create the script that triggers the active CSS class when the user is in that viewpoint
+const sections = document.querySelectorAll('section');
+const navItems = document.querySelectorAll('header nav a');
+
+//-- a onscroll event // loop // collects data
+window.onscroll = () => {
+  sections.forEach(sec => {// for each section
+  const top = window.scrollY; // This gets the scroll position 
+  const offset = sec.offsetTop; //This gets the off set top
+  const height = sec.offsetHeight; // This gets the height of the section
+  const id = sec.getAttribute('id'); // This gets the id of the section // the id include the link within the page
+
+  // checking the position
+   if (top >= offset && top < offset + height) {
+    navItems.forEach(item => {
+    item.classList.remove('active'); //remove the active when not in that section
+
+  // checking if the menu item is matching with the sections href match the id
+   if (item.getAttribute('href').slice(1) === id) {
+    item.classList.add('active'); //add when it matches the menu item
+        }
+      });
+    }
+  });
+};
+
+// When the item is press it should active
+navItems.forEach(item => { // applies to all nav item
+  item.addEventListener('click', (event) => { //when clicked//
+  event.preventDefault(); //it will prevent showing the default link behavior
+     
+  //with that we also have to remove 'active' class
+    // here i remove 'active' class from all menu bar items
+navItems.forEach(navItem => {
+navItem.classList.remove('active');
+  });
+
+  // and then, here I add 'active' class to the clicked menu bar item
+item.classList.add('active');
+
+    // When clicked it goes to the corresponding link or id
+const targetId = item.getAttribute('href').slice(1);
+const targetSection = document.getElementById(targetId);
+    
+targetSection.scrollIntoView({ behavior: 'smooth' }); // making the scroll smooth 
+  });
+});
+
+
+
+//================== ANIMATION SCROLL ================//
+
+//Similar to on scroll active menu bar on scroll 
+//here i did it for simple animation  that will only trigger when user is in the viewpoint
+
+window.addEventListener('scroll', function() { //eventlistener when scrolled
+  var aboutContentElements = document.querySelectorAll('.about-content');
+
+aboutContentElements.forEach(function(element) { //defining and getting data // this variable is to be use at if statement
+ var position = element.getBoundingClientRect();
+ var windowHeight = window.innerHeight;
+
+ //if-else statement to trigger animation if use in that position. if else it will not execute
+
+//The part where it will check if the content is visible portion of the viewpoint 
+if (position.top < windowHeight * 0.5 && position.bottom >= 0) { // to calculate if user position
+      element.classList.add('fade-in'); // if visible the CSS fade-in would show/add
+    } 
+else { 
+      element.classList.remove('fade-in');// if not visible it will hide it / remove
+    }
+  });
+});
+
+
+//Another on scroll animation for my svg // in quite different way
+window.addEventListener('scroll', function() {
+  var container = document.querySelector('.container3'); // defining variables
+  var path = container.querySelector('path');
+  var pathTop = path.getBoundingClientRect().top; // defining this to calculate the top position of path
+  var pathOffset = window.innerHeight * 0.8; // here i made a variable to show my desired position to trigger the animation
+
+ //if-else 
+ //here as the pattern i'm doing. I calculate the viewpoint with my variables
+  if (pathTop - pathOffset <= 0) {
+    container.classList.add('svg-scroll'); // If visible - add
+  } 
+  else {
+    container.classList.remove('svg-scroll'); // If not - remove
+  }
+});
+
+
+window.addEventListener('scroll', function() { //event listener when scrolled
+  var aboutContentElements = document.querySelectorAll('.box1 h2');
+
+  aboutContentElements.forEach(function(element) {
+    var position = element.getBoundingClientRect();
+    var windowHeight = window.innerHeight;
+
+    //The part where it will check if the content is visible portion of the viewpoint 
+    if (position.top < windowHeight * 0.5 && position.bottom >= 0) {
+      element.classList.add('fade-in'); // if visible the CSS fade-in would show/add
+    } 
+    else {
+      element.classList.remove('fade-in');// if not visible it will hide it / remove
+    }
+  });
+});
+
+
+/* =============== LIGHT AND DARK ===========*/
+
+// Lastly, I decided to add a toggle button for light and dark theme
+
+//Defining variable and getting it by id
+var changeThemeBtnLight = document.getElementById('theme-change-light'); //for light button / moon button
+var changeThemeBtnDark = document.getElementById('theme-change-dark'); // for dark button / sun button
+var body = document.querySelector('body');
+
+//Here im removing the dark theme in default
+changeThemeBtnDark.style.display = 'none';
+
+//Then i go on and add a eventlistener that when click
+// it will change to dark mode and the light button would change
+changeThemeBtnLight.addEventListener('click', function() { //when click
+body.classList.add('dark-theme'); // it will add 'dark-theme' // dark-theme is from my CSS
+  changeThemeBtnLight.style.display = 'none'; //i hide the light - theme button when click
+  changeThemeBtnDark.style.display = 'block'; // and add the dark - theme button
+});// in this way when the user is in dark mode,  they'll see a sun button which means pressing it would go back to light mode.
+
+
+// Vice Verse
+changeThemeBtnDark.addEventListener('click', function() {
+body.classList.remove('dark-theme');
+  changeThemeBtnDark.style.display = 'none'; //I replaced the variable
+  changeThemeBtnLight.style.display = 'block';
+});
+
+
+/*=============================== TO LET USER SEND MSG ON MY EMAIL ====================*/
+
+
+function sendEmail() { // to send the info at my email
+
+  //defining variable to be used 
+  var name = document.getElementById('name').value; // getting variables in Ids
+  var email = document.getElementById('email').value;
+  var msg = document.getElementById('msg').value;
+  //A message that well show at my email
+  var body = 'Name: ' + name + '<br/>' + 'Email: ' + email + '<br/>' + 'Subject: ' + msg + '<br/>' + 'Message: ' + msg + '<br/>' + 'Your message has been sent';
+
+  //i got this online 
+  // i used the variable that i make here
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "brixecanlas@gmail.com",
+    Password: 'rvzhxtwsiroktshi',
+    To: 'brixecanlas@gmail.com',
+    From: email,
+    Subject: "inqury mail",
+    Body: body, 
+
+    // a alert to tell the user that they have successfully sent a message
+  }).then(function(message) {
+    if (msg.trim() !== '' && email.trim() !== '' && name.trim() !== '') { //if the ids have a data
+      alert('You have successfully sent a message');//show
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
